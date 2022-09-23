a84e8d4cdcee5f07dd03ed45bf6787a0ffaf49e6
Spencer Haka <spencer.haka@gmail.com>
2022-09-22 19:35:16 -0600

    Wrote resetGameDisplay function to return to new game after timeout, and highlightWinner function to clear show which player won.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //Global Variables
 var currentGame = new Game();
+var winnerMessage;
 
 //Query Selectors
 var fighterSection = document.getElementById('fighter-section');
@@ -14,11 +15,12 @@ var changeGameButton = document.getElementById('change-game-button');
 //Event Listeners
 fighterSection.addEventListener('click', function(event) {
   currentGame.updateChoices(event.target.id);
-  gameMessage.innerText = currentGame.determineWinner();
-  playerIcon.innerText = currentGame.players[0].icon;
-  displayWin();
+  winnerMessage = currentGame.determineWinner();
+  gameMessage.innerText = winnerMessage;
   updateScore();
+  displayWin();
   show(changeGameButton);
+  setTimeout(resetGameDisplay, 3000);
 })
 
 //Main Script
@@ -38,13 +40,32 @@ function displayWin() {
       if (currentGame.players[i].choice === fightersList[j].id){
         var img = document.createElement('img');
         img.src = fightersList[j].src;
+        highlightWinner(img, i);
         choicesSection.appendChild(img);
       }
     }
   }
 }
 
+function highlightWinner(element, index) {
+  if (winnerMessage.includes('You') && index === 0) {
+    element.classList.add('winner');
+  } else if (winnerMessage.includes('Computer') && index === 1) {
+    element.classList.add('winner');
+  } else if (winnerMessage.includes('tie')){
+    return;
+  } else { element.classList.add('loser') }
+}
+
 function updateScore() {
   playerScore.innerText = currentGame.players[0].score;
   computerScore.innerText = currentGame.players[1].score;
+  console.log(currentGame.players[0].score)
+}
+
+function resetGameDisplay() {
+  show(fighterSection);
+  hide(choicesSection);
+  choicesSection.innerHTML = '';
+  gameMessage.innerText = 'Choose your fighter!';
 }
